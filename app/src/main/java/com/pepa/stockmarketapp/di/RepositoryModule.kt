3757b4ea126529596c18f1cfe55cb2e57e597cfd
Stefan <vucanstefan@gmail.com>
2024-06-28 11:55:44 +0200

    DI, Navigation(WIP)

---
+++ app/src/main/java/com/pepa/stockmarketapp/di/RepositoryModule.kt
@@ -0,0 +1,30 @@
+package com.pepa.stockmarketapp.di
+
+import com.pepa.stockmarketapp.data.csv.CSVParser
+import com.pepa.stockmarketapp.data.csv.CompanyListingsParser
+import com.pepa.stockmarketapp.data.repository.StockRepositoryImpl
+import com.pepa.stockmarketapp.domain.model.CompanyListing
+import com.pepa.stockmarketapp.domain.repository.StockRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCompanyListingsParser(
+        companyListingsParser: CompanyListingsParser
+    ): CSVParser<CompanyListing>
+
+    @Binds
+    @Singleton
+    abstract fun bindStockRepository(
+        stockRepository: StockRepositoryImpl
+    ): StockRepository
+
+}
